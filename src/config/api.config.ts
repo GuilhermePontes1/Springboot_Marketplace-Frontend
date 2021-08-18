ac5eceef119609ea5e96dfdeffb186aa1fefe79a
GuilhermePontes1 <guilhermepontes58@hotmail.com>
2021-08-18 16:28:34 -0300

    Listando categorias

---
+++ src/config/api.config.ts
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+    baseUrl: "http://localhost:8080"
+    
+}
---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../../config/api.config";
+import { CategoriaDTO } from "../../models/categoria.dto";
+//para a classe ser injetável 
+@Injectable()
+export class CategoriaService {
+
+    constructor(public http: HttpClient) {
+
+    }
+
+    findAll() : Observable<CategoriaDTO[]> {
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    }
+}
