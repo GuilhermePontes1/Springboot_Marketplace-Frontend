223f85e8b8963f139ef392ace16c09f47b27158e
GuilhermePontes1 <guilhermepontes58@hotmail.com>
2021-08-18 17:34:20 -0300

    Mostrando as categorias com amazonS3

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
-    
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://springmarketplace.s3.sa-east-1.amazonaws.com"
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
@@ -15,15 +17,19 @@ import { CategoriaService } from '../../services/domain/categoria.service';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+  
+  bucketUrl: string = API_CONFIG.bucketBaseUrl
+
+  items: CategoriaDTO[]; // ESSA vai ser a lista exposta no controlador
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public categoriaService: CategoriaService) {
   }
-
+   //Busca categorias no cliente da API
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response => {console.log(response);},
+    this.categoriaService.findAll().subscribe(response => {this.items=response},
 
     error => {
       console.log(error);
